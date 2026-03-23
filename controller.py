90a182ba8c75b8a4c2940da4a4d9446561400c22
KarakenHunter <sharmamanthan474@gmail.com>
2026-03-23 19:37:04 +0530

    feat:add command execution and response system

--- controller.py
+++ controller.py
@@ -7,3 +7,13 @@ sock.bind((host, port))
 sock.listen()
 print("[*]Waiting for connection...")
 
+client, addr=sock.accept()
+print(f"Connected to {addr}")
+while True:
+    cmd=input("~$ ")
+    if cmd.lower=='quit':
+        print("Connection closed successfully!")
+    else:
+        client.send(cmd.encode('utf-8'))
+        resp=client.recv(4096)
+        print(resp.decode('utf-8'))
